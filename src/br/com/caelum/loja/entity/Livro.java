ab6602ec153d61a65a1a49fca23f28d74555c87d
jpfaria@gmail.com <jpfaria@gmail.com>
2013-06-25 19:15:05 -0300

    .

--- src/br/com/caelum/loja/entity/Livro.java
+++ src/br/com/caelum/loja/entity/Livro.java
@@ -1,11 +1,14 @@
 package br.com.caelum.loja.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Livro implements Serializable {
@@ -17,8 +20,12 @@ public class Livro implements Serializable {
 	private Long id;
 
 	private String nome;
+	
 	private double preco;
 
+	@ManyToMany
+	private List<Autor> autores = new ArrayList<Autor>();
+	
 	public Long getId() {
 		return id;
 	}
@@ -41,6 +48,14 @@ public class Livro implements Serializable {
 
 	public void setPreco(double preco) {
 		this.preco = preco;
+	}
+	
+	public List<Autor> getAutores() {
+		return autores;
+	}
+
+	public void setAutores(List<Autor> autores) {
+		this.autores = autores;
 	}
 
 	@Override
