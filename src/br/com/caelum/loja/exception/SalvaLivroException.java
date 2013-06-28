11a9647e6cf6daf68a30f4cdba6b97bd2d816b48
João Paulo Faria <jpfaria@gmail.com>
2013-06-28 01:41:13 -0300

    .

--- src/br/com/caelum/loja/exception/SalvaLivroException.java
+++ src/br/com/caelum/loja/exception/SalvaLivroException.java
@@ -2,6 +2,7 @@ package br.com.caelum.loja.exception;
 
 import javax.ejb.ApplicationException;
 
+@SuppressWarnings("serial")
 @ApplicationException(rollback=true)
 public class SalvaLivroException extends RuntimeException {
 
