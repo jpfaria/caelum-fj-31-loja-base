6dcbb5a65e75f685f0f688e88063c42787b36e97
jpfaria <jpfaria@gmail.com>
2013-07-01 16:31:05 -0300

    .

--- src/br/com/caelum/loja/session/GerenciadorLoja.java
+++ src/br/com/caelum/loja/session/GerenciadorLoja.java
@@ -2,9 +2,14 @@ package br.com.caelum.loja.session;
 
 import java.util.List;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
 import br.com.caelum.loja.entity.Autor;
 import br.com.caelum.loja.entity.Livro;
 
+@Path("/livraria")
 public interface GerenciadorLoja {
 
 	Livro procura(String isbn);
@@ -17,6 +22,9 @@ public interface GerenciadorLoja {
 	
 	List<Autor> getAutoresDoLivro(Livro livro);
 	
+	@GET
+	@Path("/livros/autor/{nome}")
+	@Produces({"application/xml", "application/json"})	
 	List<Livro> buscaLivrosPeloNomeDoAutor(String nome);
 	
 	List<Livro> listaLivros();
